f8858aaf96b541736f4ce1be48dc4c8846a21b03
tihonove <tihonove@skbkontur.ru>
2017-07-29 20:59:17 +0500

    update eslint to 4.3.0

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
+const commonRules = require("./rules");
+
 module.exports = {
-    "extends": [
-        require.resolve("./base.js"),
-        require.resolve("./react.js"),
-    ],
-    "rules": {},
-    "globals": {
-        "$Shape": false,
-    }
+    parser: "babel-eslint",
+    plugins: ["react", "babel", "flowtype", "prettier", "import"],
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
+        node: true,
+    },
+    parserOptions: {
+        sourceType: "module",
+        ecmaFeatures: {
+            modules: true,
+            jsx: true,
+        },
+    },
+    settings: {
+        flowtype: {
+            onlyFilesWithFlowAnnotation: true,
+        },
+    },
+    rules: Object.assign(
+        {},
+        {
+            "prettier/prettier": [
+                "error",
+                {
+                    printWidth: 120,
+                    tabWidth: 4,
+                    useTabs: false,
+                    semi: true,
+                    singleQuote: false,
+                    trailingComma: "es5",
+                    bracketSpacing: true,
+                    jsxBracketSameLine: true,
+                },
+            ],
+        },
+        commonRules
+    ),
 };
